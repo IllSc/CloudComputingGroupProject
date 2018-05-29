8a0bb02dd46e9af548f041a223c6e9fbf334c1e2
jayanthi456 <jayanthi.kambayatughar@ulb.ac.be>
2018-05-29 19:16:05 +0200

    added html and css files

--- helloworld/application.py
+++ helloworld/application.py
@@ -2,6 +2,7 @@
 import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
+from flask import render_template
 
 application = Flask(__name__)
 
@@ -16,16 +17,7 @@ def post():
 
 @application.route('/')
 def homepage():
-	return """
-		<h1>Hello world!</h1>
-
-		<iframe src="https://search-cloud-computing-project-upc-uq5b7q3o57i5xaez54rlfluabu.eu-central-1.es.amazonaws.com/_plugin/kibana/app/kibana#/visualize/edit/3075e1e0-5fa3-11e8-b28b-e5ea3f49db77?embed=true&_g=()" height="600" width="800"></iframe>
-		
-		<iframe src="https://search-cloud-computing-project-upc-uq5b7q3o57i5xaez54rlfluabu.eu-central-1.es.amazonaws.com/_plugin/kibana/app/kibana#/visualize/edit/3075e1e0-5fa3-11e8-b28b-e5ea3f49db77?embed=true&_g=()" height="600" width="800"></iframe>
-		<iframe src="https://search-cloud-computing-project-upc-uq5b7q3o57i5xaez54rlfluabu.eu-central-1.es.amazonaws.com/_plugin/kibana/app/kibana#/visualize/edit/9db1a9c0-5fac-11e8-b28b-e5ea3f49db77?embed=true&_g=()" height="600" width="800"></iframe>
-		<iframe src="https://search-cloud-computing-project-upc-uq5b7q3o57i5xaez54rlfluabu.eu-central-1.es.amazonaws.com/_plugin/kibana/app/kibana#/visualize/edit/e62f3990-5fa4-11e8-b28b-e5ea3f49db77?embed=true&_g=()" height="600" width="800"></iframe>
-		<iframe src="https://search-cloud-computing-project-upc-uq5b7q3o57i5xaez54rlfluabu.eu-central-1.es.amazonaws.com/_plugin/kibana/app/kibana#/visualize/edit/eabcf380-5fae-11e8-b28b-e5ea3f49db77?embed=true&_g=()" height="600" width="800"></iframe>
-		<iframe src="https://search-cloud-computing-project-upc-uq5b7q3o57i5xaez54rlfluabu.eu-central-1.es.amazonaws.com/_plugin/kibana/app/kibana#/visualize/edit/82eb3cd0-5fa4-11e8-b28b-e5ea3f49db77?embed=true&_g=()" height="600" width="800"></iframe>
-		"""
+	
+	return render_template('template.html')
 if __name__ == '__main__':
     flaskrun(application)
