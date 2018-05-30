25d73b5bf32f4741eb068361cbdf79c11589b1e9
jayanthi456 <jayanthi.kambayatughar@ulb.ac.be>
2018-05-30 11:22:53 +0200

    updated application.py

--- helloworld/application.py
+++ helloworld/application.py
@@ -3,8 +3,13 @@ import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
 from flask import render_template
+from flask_cdn import CDN
 
 application = Flask(__name__)
+application.config['CDN_DOMAIN'] = 'd1r1rtixv3o3sw.cloudfront.net'
+application.config['CDN_DEBUG'] = True
+application.config['CDN_HTTPS'] = True
+CDN(application)
 
 """@application.route('/', methods=['GET'])
 def get():
@@ -17,7 +22,6 @@ def post():
 
 @application.route('/')
 def homepage():
-	
 	return render_template('template.html')
 if __name__ == '__main__':
     flaskrun(application)
