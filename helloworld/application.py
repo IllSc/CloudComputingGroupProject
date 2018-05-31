268f9b5670e13100636af5616150bc48e4c1844e
Ivan Putera Masli <ivanmasli@gmail.com>
2018-05-31 04:19:30 +0200

    Remove CDN

--- helloworld/application.py
+++ helloworld/application.py
@@ -3,14 +3,8 @@ import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
 from flask import render_template
-from flask_cdn import CDN
 
 application = Flask(__name__)
-application.config['CDN_DOMAIN'] = 'd1r1rtixv3o3sw.cloudfront.net'
-application.config['CDN_DEBUG'] = True
-application.config['CDN_HTTPS'] = True
-CDN(application)
-
 """@application.route('/', methods=['GET'])
 def get():
     return Response(json.dumps({'Output': 'Hello jayanthi'}), mimetype='application/json', status=200)
